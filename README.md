5a7ec9a9ee5f9ff2f12b820dac5f495b5a6682b1
Roger Simms <roger.simms@gmail.com>
2015-07-12 12:55:21 +0100

    Get working with Redactor v10 & performance updates
    * Now automatically works without messing about with redactors callbacks
    * Use key codes to identify characters for performance boost
    * Mix of keypress and keydown events for checking & editing needs
    * Try to work around bug where spans and wierd `&#8203;` character is
      added.

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 redactor-auto-lists
 ===================
 
-Simple plugin for redactor js html editor (http://imperavi.com/redactor/)
+*Simple plugin for redactor JavaScript html editor (http://imperavi.com/redactor/).*
+
+Creates lists from text input, rather than having to use the menu bar; just like your favourite word processor or markdown syntax. Works on list conventions of '-', '*' for an unordered list and '1.' to start an ordered list.
+
+## To install
+
+Just add to the plugin as per docs:
+
+```javascript
+
+$(function() {
+  $('#content').redactor({
+    focus: true,
+    plugins: ['autoLists'],
+  });
+});
+
+```
--- redactor_auto_list.js
+++ redactor_auto_list.js
@@ -1,38 +1,106 @@
-/**
- * Plugin to generate an unorderd list on use of ‘-’ or ‘*’, will also
- * create an ordered list of keying ‘1.’.
- */
+(function($) {
+  $.Redactor.prototype.autoLists = function() {
 
-if (typeof RedactorPlugins === 'undefined') var RedactorPlugins = {};
+    // Valid keyCodes: '*', '-', '1', '.'
+    var keyCodes   = [42, 45, 49, 46];
+    var prevKey    = false;
+    var action     = false;
 
-RedactorPlugins.myPlugins = {
-  autoLists: function(e) {
-    if (e.which != 32) return;  // Check if current charater is a space
-    var c = this.getBlock();  // Get existing content of this block
-    var t = (c.innerText || c.textContent).trim()  // Get the string from the html
-    if (t.length > 2) return;  // Forget anything over 3 characters
-    var l = '';
-    switch(t) {
-      case '*':
-      case '-':
-        l = 'insertunorderedlist'
-        break;
-      case '1.':
-        l = 'insertorderedlist'
-        break;
-      default:
-        return;
+
+    /**
+     * Registers the plugin with redactor.
+     *
+     * @return {void}
+     */
+    function init() {
+      this.$editor.on('keypress', $.proxy(keyHandler, this));
+      this.$editor.on('keydown', $.proxy(spaceHandler, this));
     }
-    this.exec(l);
-    this.getBlock().innerHTML = "";
-    e.preventDefault();
-  }
-}
 
-// Use with something like:
-// $('#body').redactor({
-//   plugins: ['myPlugins'],
-//   keydownCallback: function(e) {
-//     this.autoLists(e);
-//   },
-// });
+
+    /**
+     * Triggers the toggle list action.
+     *
+     * @param {window.event} ev
+     * @return {void}
+     */
+    function spaceHandler(ev) {
+      if (ev.which !== 32 || action === false) return;
+
+      var strLen = currentStrLen(this.selection.getCurrent());
+
+      if ((strLen !== 1 && action === 'unorderedlist') || (strLen !== 2 && action === 'orderedlist')) {
+        action = false;
+        return;
+      }
+
+      ev.preventDefault();
+
+      this.list.toggle(action);
+      var el = this.selection.getCurrent();
+
+      // Get rid of the span that redactor can add
+      while (el.firstChild) el.removeChild(el.firstChild);
+
+      // Safari didn't want to work with jQuery’s empty()/text('') so: javascript
+      (el.textContent !== undefined) ? el.textContent = '': el.innerText = '';
+
+      action = false;
+    }
+
+
+    /**
+     * Figurs out what sort of list we want to make
+     *
+     * @param {window.event} ev
+     * @return {void}
+     */
+    function keyHandler(ev) {
+      var key = ev.which;
+
+      if ($.inArray( key, keyCodes) === -1 && key !== 32) {
+        prevKey = false;
+        return;
+      }
+
+      if (currentStrLen(this.selection.getCurrent()) > 1) return;
+
+      switch (key) {
+        case 42:
+        case 45:
+          action = 'unorderedlist';
+          break;
+        case 49:
+         prevKey = 49;
+         break;
+        case 46:
+          if (prevKey !== 49) return;
+          action = 'orderedlist';
+          prevKey = false;
+          break;
+        default:
+          return;
+      }
+      return;
+    }
+
+
+    /**
+     * Simple helper to determine the string leangth of the content.
+     *
+     * NOTE: the editor will add the `&#8203;` character, e.g. after
+     * closing previous lists. We need to check for it.
+     *
+     * @param {DOM element} el
+     * @return {integer}
+     */
+    function currentStrLen(el) {
+      return $(el).text().replace(/[\u200B]/g, '').trim().length;
+    }
+
+
+    return {
+        init: init
+    };
+  };
+})($);
